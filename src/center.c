ddca7ed1de855ab8ab063e854fea0af3f5e5f684
turquoise-hexagon <camille@lzr.pw>
2020-09-01 15:34:03 +0200

    refactor code

--- src/center.c
+++ src/center.c
@@ -1,6 +1,5 @@
 #include <sys/ioctl.h>
 
-#include <err.h>
 #include <limits.h>
 #include <signal.h>
 #include <stdio.h>
@@ -9,27 +8,56 @@
 #include <string.h>
 #include <unistd.h>
 
-/*
- * handling parameters with signal handles is complicated
- * we're gonna use global variables here, since the program is short
- */
-static size_t x_size = 0;
-static size_t y_size = 0;
-static size_t file_length = 0;
-static char **file_content = NULL;
+static size_t x            = 0;
+static size_t y            = 0;
+static size_t content_size = 0;
+static char **content      = NULL;
 
-/*
- * helper functions
- */
 static inline void *
 allocate(size_t size)
 {
     void *ptr;
 
-    if ((ptr = malloc(size)) == NULL)
-        errx(1, "failed to allocate memory");
+    if (! (ptr = malloc(size))) {
+        fprintf(stderr, "error : failed to allocate memory\n");
+
+        exit(1);
+    }
 
     return ptr;
+}
+
+static void *
+reallocate(void *old, size_t size)
+{
+    void *new;
+
+    if (! (new = realloc(old, size))) {
+        fprintf(stderr, "error : failed to reallocate memory\n");
+
+        exit(1);
+    }
+
+    return new;
+}
+
+static inline char *
+copy_input(const char *str)
+{
+    char *copy;
+
+    {
+        size_t len;
+        
+        len = strnlen(str, LINE_MAX);
+        copy = allocate(len * sizeof(*copy));
+        strncpy(copy, str, len);
+
+        /* fix string */
+        copy[len - 1] = 0;
+    }
+
+    return copy;
 }
 
 static FILE *
@@ -37,8 +65,11 @@ open_file(const char *path, const char *mode)
 {
     FILE *file;
 
-    if ((file = fopen(path, mode)) == NULL)
-        errx(1, "failed to open '%s'", path);
+    if (! (file = fopen(path, mode))) {
+        fprintf(stderr, "error : failed to open '%s'\n", path);
+
+        exit(1);
+    }
 
     return file;
 }
@@ -46,8 +77,11 @@ open_file(const char *path, const char *mode)
 static void
 close_file(const char *path, FILE *file)
 {
-    if (fclose(file) == EOF)
-        errx(1, "failed to close '%s'", path);
+    if (fclose(file) != 0) {
+        fprintf(stderr, "error : failed to close '%s\n'", path);
+
+        exit(1);
+    }
 }
 
 static void
@@ -55,11 +89,52 @@ get_terminal_size(void)
 {
     struct winsize window;
 
-    if (ioctl(fileno(stdout), TIOCGWINSZ, &window) == -1)
-        errx(1, "failed to get terminal size");
+    if (ioctl(fileno(stdout), TIOCGWINSZ, &window) == -1) {
+        fprintf(stderr, "error : failed to get terminal size\n");
 
-    x_size = window.ws_row;
-    y_size = window.ws_col;
+        exit(1);
+    }
+
+    x = window.ws_row;
+    y = window.ws_col;
+}
+
+static void
+load_content(const char *path)
+{
+    FILE *file;
+
+    file = open_file(path, "r");
+
+    {
+        size_t allocated = 2;
+        size_t assigned  = 0;
+
+        content = allocate(allocated * sizeof(*content));
+
+        char input[LINE_MAX] = {0};
+
+        while (fgets(input, LINE_MAX, file)) {
+            content[assigned] = copy_input(input);
+
+            if (++assigned == allocated)
+                content = reallocate(content,
+                    (allocated = allocated * 3 / 2) * sizeof(*content));
+        }
+
+        content_size = assigned;
+    }
+
+    close_file(path, file);
+}
+
+static void
+cleanup_content(void)
+{
+    for (size_t i = 0; i < content_size; ++i)
+        free(content[i]);
+
+    free(content);
 }
 
 static void
@@ -72,99 +147,58 @@ install_signal_handler(void (*function)(int), int signal)
     sig.sa_flags = SA_RESTART;
     sig.sa_handler = function;
 
-    if (sigaction(signal, &sig, NULL) == -1)
-        errx(1, "failed to install signal handler");
-}
+    if (sigaction(signal, &sig, NULL) == -1) {
+        fprintf(stderr, "error : failed to install signal handler\n");
 
-/*
- * main functions
- */
-static void
-load_file(const char *path)
-{
-    FILE *file;
-
-    file = open_file(path, "r");
-
-    char line[LINE_MAX] = {0};
-    size_t allocated_size = 1;
-
-    file_content = allocate(allocated_size * sizeof(*file_content));
-
-    for (;;) {
-        if (fgets(line, LINE_MAX, file) == NULL)
-            break;
-
-        file_content[file_length] = allocate(LINE_MAX *
-            sizeof(*file_content[file_length]));
-
-        strncpy(file_content[file_length], line, LINE_MAX);
-
-        if (++file_length == allocated_size)
-            if ((file_content = realloc(file_content,
-                    (allocated_size *= 2) * sizeof(*file_content))) == NULL)
-                errx(1, "failed to allocate memory");
+        exit(1);
     }
-
-    close_file(path, file);
 }
 
 static void
 draw_content(int signal)
 {
-    (void)signal; /* throw away signal */
+    /* throw signal away */
+    (void)signal;
 
     /*
-     * 2J : clear terminal
-     *  H : start printing in (0, 0)
+     * ?25l : hide cursor
+     * 2J   : clear terminal
+     * x;yH : move cursor to x;y
      */
-    printf("\033[2J\033[H");
-
     get_terminal_size();
+    printf("\033[?25l\033[2J\033[H");
+    printf("\033[%ldH", (x - content_size) / 2);
 
-    /* align vertically */
-    for (size_t i = 0; i < (x_size - file_length) / 2; ++i)
-        putchar('\n');
+    for (size_t i = 0; i < content_size; ++i) {
+        size_t len;
 
-    /* align horizontally */
-    size_t length;
-
-    for (size_t i = 0; i < file_length; ++i) {
-        length = strnlen(file_content[i], LINE_MAX);
-
-        printf("%*s", (int)(y_size + length) / 2, file_content[i]);
+        len = strnlen(content[i], LINE_MAX);
+        printf("%*s\n", (int)(y + len) / 2, content[i]);
     }
 }
 
 static noreturn void
 cleanup(int signal)
 {
-    (void)signal; /* throw away signal */
+    /* throw signal away */
+    (void)signal;
 
     /* ?25h : show cursor */
     printf("\033[?25h");
-
-    for (size_t i = 0; i < file_length; ++i)
-        free(file_content[i]);
-
-    free(file_content);
-
+    cleanup_content();
     exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
-    load_file(argc != 1 ? argv[1] : "/dev/stdin");
-
-    /* ?25l : hide cursor */
-    printf("\033[?25l");
+    load_content(argc != 1 ? argv[1] : "/dev/stdin");
 
     draw_content(0);
 
     install_signal_handler(cleanup, SIGINT);
     install_signal_handler(draw_content, SIGWINCH);
 
-    for (;;) /* keep the program busy */
+    for (;;)
         sleep(10);
 }
